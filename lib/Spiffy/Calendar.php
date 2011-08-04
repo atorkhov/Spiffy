666eceba2050b635f2602f4f44a4d388efa10019
Kyle Spraggs <theman@spiffyjr.me>
2011-08-03 21:44:45 -0500

    Updated license information

--- lib/Spiffy/Calendar.php
+++ lib/Spiffy/Calendar.php
@@ -1,4 +1,20 @@
 <?php
+/**
+* Spiffy Framework
+*
+* LICENSE
+*
+* This source file is subject to the new BSD license that is bundled
+* with this package in the file LICENSE.
+* It is also available through the world-wide-web at this URL:
+* http://www.spiffyjr.me/license
+*
+* @category   Spiffy
+* @package    Spiffy
+* @copyright  Copyright (c) 2011 Kyle Spraggs (http://www.spiffyjr.me)
+* @license    http://www.spiffyjr.me/license     New BSD License
+*/
+
 namespace Spiffy;
 use Zend_Date, Zend_Locale, Spiffy\Calendar\Render\Exception;
 
--- lib/Spiffy/Form/Element/Entity.php
+++ lib/Spiffy/Form/Element/Entity.php
@@ -1,4 +1,19 @@
 <?php
+/**
+* Spiffy Framework
+*
+* LICENSE
+*
+* This source file is subject to the new BSD license that is bundled
+* with this package in the file LICENSE.
+* It is also available through the world-wide-web at this URL:
+* http://www.spiffyjr.me/license
+*
+* @category   Spiffy
+* @package    Spiffy_Form
+* @copyright  Copyright (c) 2011 Kyle Spraggs (http://www.spiffyjr.me)
+* @license    http://www.spiffyjr.me/license     New BSD License
+*/
 
 use Doctrine\ORM\EntityRepository;
 use Doctrine\ORM\QueryBuilder;
