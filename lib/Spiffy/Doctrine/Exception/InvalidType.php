6ce479ed8acec1e20f6d25c59b742fa9456cb9e8
Kyle Spraggs <kyle.spraggs@avimark.net>
2011-08-30 09:02:18 -0500

    Added InvalidType.

---
+++ lib/Spiffy/Doctrine/Exception/InvalidType.php
@@ -0,0 +1,27 @@
+<?php
+/**
+* Spiffy Framework
+*
+* LICENSE
+*
+* This source file is subject to the new BSD license that is bundled
+* with this package in the file LICENSE.
+* It is also available through the world-wide-web at this URL:
+* http://www.spiffyjr.me/license
+*
+* @category   Spiffy
+* @package    Spiffy_Doctrine
+* @copyright  Copyright (c) 2011 Kyle Spraggs (http://www.spiffyjr.me)
+* @license    http://www.spiffyjr.me/license     New BSD License
+*/
+
+namespace Spiffy\Doctrine\Exception;
+use Zend_Exception;
+
+class InvalidType extends Zend_Exception
+{
+    public function __construct($msg = '')
+    {
+        parent::__construct($msg, 500);
+    }
+}
